7462f209d5a9c8d8058709aba99a930144c890bd
Jihanebgit <jihanebattas025@gmail.com>
2026-04-21 18:14:37 +0200

    ITER1 new

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -3,28 +3,18 @@ package tdd;
 public class Game {
 
 	int score=0;
-	//chaque fois que le joueur lance boule appel
+	
 	void roll(int nbQuilles) {
 		score+=nbQuilles;
 	}
 	
-	//score total partie 
 	int score() {
 		return score;
 	}
 	
-	void nbLancers(int nbLancers, int nbQuilles) {
-		for (int i=0; i<nbLancers; i++) {
+	void nbLancers(int nbQuilles, int n) {
+		for (int i=0; i<n;i++) {
 			roll(nbQuilles);
 		}
 	}
-	
-	void spare(int nbQuilles) {
-		score+=2*nbQuilles;
-	}
-	
-	void strike(int nbQuillesTour1, int nbQuillesTour2) {
-		score+=2*nbQuillesTour1+2*nbQuillesTour2;
-	}
-	
 }
--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -8,92 +8,30 @@ import org.junit.jupiter.api.Test;
 class GameTest {
 
 	Game game;
-	@BeforeEach
-	void setup() {
-		game=new Game();
-	}
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		game= new Game();
+	}
+
 	@Test
 	void test_Niveau1() {
-		//vide
 		assertEquals(0, game.score());
 		
-		//20 roll de 0
-		game.nbLancers(20, 0);
+		//cas 20 lancers de 0
+		game.nbLancers(0, 20);
 		assertEquals(0, game.score());
 		
-		//20 roll de 1
-		game.nbLancers(20, 1);
+		//cas 20 lancers de 1
+		game.nbLancers(1, 20);
 		assertEquals(20, game.score());
 		
-		//nouveau jeu
+		//cas 10 lancers de 1 et 10 lancers de 2
 		game.score=0;
-		
-		//10 roll de 1 + 10 roll de 2
-		game.nbLancers(10, 1);
-		game.nbLancers(10, 2);
-		
+		game.nbLancers(1, 10);
+		game.nbLancers(2, 10);
 		assertEquals(30, game.score());
 		
 	}
-	
-	@Test
-	void test_spare() {
-		//spare
-		game.nbLancers(1,7);
-		game.nbLancers(1,3);
-		//bonus
-		game.spare(4);
-		//autres tour
-		game.nbLancers(17,0);
-		assertEquals(18, game.score());
-	}
-	
-	@Test
-	void test_strike() {
-		game.nbLancers(1,10);
-		game.strike(3, 4);
-		game.nbLancers(16,0);
-		assertEquals(24, game.score());
-	}
-	
-	@Test 
-	void test_casGeneral() {
-		int nbLancer=19;
-		while (nbLancer>19) {
-			
-		}
-		game.roll(1);
-		game.roll(2);
-		
-		game.roll(10); //strike 
-		
-		game.strike(0, 10); //spare
-		
-		game.spare(4);
-		game.roll(2);
-		
-		game.roll(0);
-		game.roll(10); //spare
-		
-		game.spare(6);
-		game.roll(2);
-	
-		game.roll(0);
-		game.roll(10); //spare
-		
-		game.spare(6);
-		game.roll(4); //spare
-		
-		game.spare(8);
-		game.roll(2);//spare
-		
-		game.spare(2);
-		game.roll(7);
-		
-		assertEquals(122, game.score());
-	}
-
-	
 
 }
